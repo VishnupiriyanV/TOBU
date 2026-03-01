07ef4184437028c8421b4756ae9b80080eb1c3e5
Nishanth <nishanthscontact@gmail.com>
2026-03-01 22:27:24 +0530

    Add /query endpoint to backend.py and sqlite
    Creates sqlite backend.db for saving queries

--- backend/backend.py
+++ backend/backend.py
@@ -1,3 +1,4 @@
+import sqlite3
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/query")
+def query():
+    try:
+        with sqlite3.connect("backend.db") as conn:
+            pass
+    except sqlite3.OperationalError as e:
+        print("Failed to open database:", e)
