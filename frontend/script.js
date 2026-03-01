d6ac06534bc0c10530abccf40d7d018da0dc48bf
Nishanth <nishanthscontact@gmail.com>
2026-03-02 00:43:24 +0530

    Previous commit message,forgot to commit the files

---
+++ frontend/script.js
@@ -0,0 +1,23 @@
+// Please dont touch this file, took me hours
+
+// Send the form data to the fastapi server /query
+var form = document.getElementById('search-form');
+form.onsubmit = function(event){
+        var formData = new FormData(form);
+        const response = fetch("http://127.0.0.1:8000/query", {
+            method: "POST",
+            headers: {
+                "accept": "application/json",
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(Object.fromEntries(formData)),
+            })
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(error => console.log('Failed: ' + error.message));
+        window.location.replace("search.html");
+        //Fail the onsubmit to avoid page refresh.
+        return false; 
+    }
+
+
