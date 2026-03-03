f43ee31c9c1224c10221d3586d658a89bace4d4d
VishnupiriyanV <vishnupiriyan086@gmail.com>
2026-03-04 01:45:37 +0530

    Fix save_to_vector_db function definition and add data mapping logic

--- backend/search_and_index/semantic_engine.py
+++ backend/search_and_index/semantic_engine.py
@@ -1,5 +1,6 @@
 
 from sentence_transformers import SentenceTransformer
+import lancedb
 
 
 #converts text to embedding
@@ -8,4 +9,50 @@ def embed(sentences):
     embeddings = model.encode(sentences)
     return embeddings
 
-def save_to_vector_db(media_id)
+
+
+
+def save_to_vector_db(media_id,file_name,file_path,transcript_data,db_path="vector_data"):
+    
+    windowed_text_lists = sentence_window(transcript_data)
+
+    texts_to_embed = [" ".join(window) for window in windowed_text_lists]
+
+    #generate the embedding
+    vector_matrix = embed(texts_to_embed)
+    embeddings = vector_matrix.tolist()
+
+    #map data
+
+    data = []
+    for i in range(len(transcript_data)):
+        data.append({
+            "vector" : embeddings[i],
+            "text" : transcript_data[i]["text"],
+            "context": texts_to_embed[i],
+            "start" : transcript_data[i]["start"]
+            "file_name": file_name,
+            "file_path":file_path,
+            "media_id" : media_id
+
+        })
+
+    # storing the data in lancedb
+
+    db = lancedb.connect(db_path)
+    table_name = "semantic_segments"
+
+    if table_name in db.table_name():
+        table = db.open_table(table_name)
+        table.add(data)
+    else:
+        db.create_table(table_name,data=data)
+
+
+
+    
+
+
+
+
+
