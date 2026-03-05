cc814fd54b53d2bed8b47171544c546728f240a4
VishnupiriyanV <vishnupiriyan086@gmail.com>
2026-03-05 12:32:27 +0530

    Implement video frame extraction and thumbnail generation in index_video_visually function

--- backend/search_and_index/visual_engine.py
+++ backend/search_and_index/visual_engine.py
@@ -3,11 +3,62 @@ from PIL import Image
 import os
 from semantic_engine import VECTOR_DB_PATH
 
+
+INTERVAL_SECONDS =2 #frame extraction 
+BATCH_SIZE = 50 #50 frames cap for storing before saving in the DB
 THUMBNAIL_PATH = os.path.join("backend", "search_and_index", "tempfile", "thumbnails")
+THUMBNAIL_MAX_SIZE= (320,320)
+THUMBNAIL_QUALITY = 80
+
 
 def  index_video_visually(video_path,media_id,db_path=VECTOR_DB_PATH,):
 
     if not os.path.exists(THUMBNAIL_PATH):
         os.makedirs(THUMBNAIL_PATH)
+
+    cap = cv2.VideoCapture(video_path)
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    interval = int(fps*) #extract one frame per second
+
+    frames_batch = []
+    count =0
+    batch_size = BATCH_SIZE
+
+    while cap.isOpened():
+        ret,frame = cap.read()
+
+        if not ret:
+            break
+
+        if count % interval == 0:
+
+            #converts BGR to RGB since ai models and pil use rgb
+
+            colour_converted = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            pil_img = Image.fromarray(colour_converted)
+
+            timestamp = round(count/fps,2)
+            thumb_filename = f"{media_id}_{timestamp}.jpg"
+            thumb_path = THUMBNAIL_PATH
+
+            pil_img.thumnail(THUMBNAIL_MAX_SIZE)
+            pil_img.save(thumb_path,"jpeg",quality=THUMBNAIL_QUALITY )
+
+            #placeholder for model embedding
+
+
+
+
+
+
+
+            
+            
+
+            
+
+
 
     
