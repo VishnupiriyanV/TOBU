a01fc02abfcd114ecffc1ed96ca0b5f0c34d417d
VishnupiriyanV <vishnupiriyan086@gmail.com>
2026-03-11 01:47:00 +0530

    Refactor aural_engine.py: remove unused imports and main execution block; add main.py for audio processing workflow

--- backend/search_and_index/aural_engine.py
+++ backend/search_and_index/aural_engine.py
@@ -1,18 +1,10 @@
 import ffmpeg
 import json
-import warnings
 from faster_whisper import WhisperModel #needs cuda_12 toolkit for gpu
-import time
 import os
-from sql_database import save_to_db, search_to_json, initialize_db,DATABASE_PATH
-from semantic_engine import save_to_vector_db, save_summary_vector
-import sqlite3
-from summarizer import summary_generator
 import uuid
-from visual_engine import index_video_visually
 
 
-warnings.filterwarnings("ignore")
 
 
 
@@ -75,53 +67,3 @@ def get_duration(path):
     probe = ffmpeg.probe(path)
     duration = float(probe["format"]["duration"])
     return duration
-
-
-if __name__ == "__main__":
-    
-    path = "C:/Helm/FossHack/TOBU/test3.mp4"
-    start = time.time()
-
-    audio_path = extract_audio(path)
-    file_name = get_file_name(path)
-    duration = get_duration(path)
-
-
-    if audio_path:
-        transcript = transcribe_audio(audio_path)
-        summary_text = summary_generator(transcript)
-
-        connection = sqlite3.connect(DATABASE_PATH)
-        initialize_db()
-
-        
-        cursor = connection.cursor()
-        cursor.execute("SELECT id FROM media_files WHERE file_path = ?", (path,))
-        result = cursor.fetchone()
-
-        if result:
-            media_id = result[0]
-            print(f"File already exists in DB with media_id: {media_id}")
-        else:
-            media_id = save_to_db(path, file_name, duration, transcript, summary=summary_text)
-            if media_id is None:
-                print("Failed to save to database")
-                connection.close()
-                exit(1)
-
-        connection.close()
-
-        save_to_vector_db(media_id, file_name, path, transcript, summary=summary_text)
-        save_summary_vector(media_id, file_name, summary_text)
-    
-        print("visual indexing starts:")
-        index_video_visually(path, media_id)
-        
-        
- 
-
-
-
-    end = time.time()
-
-    print("Execution time:",end-start)
